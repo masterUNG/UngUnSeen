24ce0b6ec28e6a6974707bb46bd6fc6aaeafc3a7
masterUNG <phrombutr@gmail.com>
2019-09-30 13:41:41 +0700

    Camera and Gallery Success

--- lib/screens/form_page.dart
+++ lib/screens/form_page.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class FormPage extends StatefulWidget {
   @override
@@ -6,8 +9,77 @@ class FormPage extends StatefulWidget {
 }
 
 class _FormPageState extends State<FormPage> {
+  // Explicit
+  File file;
+
+  // Method
+  Widget showImage() {
+    return Container(
+      margin: EdgeInsets.only(top: 50.0),
+      height: 200.0,
+      child: file == null ? Image.asset('images/pic.png') : Image.file(file),
+    );
+  }
+
+  Widget showButton() {
+    return Container(
+      width: 200.0,
+      // color: Colors.grey,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          cameraButton(),
+          galleryButton(),
+        ],
+      ),
+    );
+  }
+
+  Widget cameraButton() {
+    return IconButton(
+      icon: Icon(
+        Icons.add_a_photo,
+        size: 48.0,
+        color: Colors.purple.shade400,
+      ),
+      onPressed: () {
+        cameraThread();
+      },
+    );
+  }
+
+  Future<void> cameraThread()async{
+
+    var imageObject = await ImagePicker.pickImage(source: ImageSource.camera);
+    setState(() {
+      file = imageObject;
+    });
+
+  }
+
+  Widget galleryButton() {
+    return IconButton(
+      icon: Icon(Icons.add_photo_alternate, size: 48.0,color: Colors.green,),
+      onPressed: () {
+        galleryThread();
+      },
+    );
+  }
+
+  Future<void> galleryThread()async{
+    var imageObject = await ImagePicker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      file = imageObject;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('This is Form Page');
+    return ListView(
+      children: <Widget>[
+        showImage(),
+        showButton(),
+      ],
+    );
   }
 }
